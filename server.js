66a63835f02d29f678aee8aafb312f1592cbcbc6
ooxi <violetland@mail.ru>
2012-12-07 00:31:55 +0100

    History development...

--- server.js
+++ server.js
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-var EtherDraw = require('etherdraw');
+var EtherDraw = require('./etherdraw.js');
 
 
 
