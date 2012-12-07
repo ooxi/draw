ff6dc99ee909d5582854cb16a41d2a1d24db96ec
ooxi <violetland@mail.ru>
2012-12-07 01:20:53 +0100

    History development...

--- src/static/js/etherdraw.sketch.js
+++ src/static/js/etherdraw.sketch.js
@@ -20,10 +20,34 @@
 
 
 /**
- * Represents one sketch
+ * Represents a sketch
  */
 var EtherDraw = EtherDraw || {};
 
 EtherDraw.Sketch = EtherDraw.Sketch || function(id, dom) {
+
+  // Get a reference to the canvas object
+  var canvas = document.getElementById('myCanvas');
+
+  // Create an empty project and a view for the canvas:
+  paper.setup(canvas);
+
+  // Create a Paper.js Path to draw a line into it:
+  var path = new paper.Path();
+
+  // Give the stroke a color
+  path.strokeColor = 'black';
+  var start = new paper.Point(100, 100);
+
+  // Move to start and draw a line from there
+  path.moveTo(start);
+
+  // Note that the plus operator on Point objects does not work
+  // in JavaScript. Instead, we need to call the add() function:
+  path.lineTo(start.add([ 200, -50 ]));
+
+  // Draw the view now:
+  paper.view.draw();
+
 };
 
